c90a9e478d114b946d2be7bdbdcbaf9854131c21
Basantakhadka <basantakhadka963@gmail.com>
2026-06-22 10:41:12 +0545

    remove tenchnical stuff

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,8 @@ import auth from "@app/config/authentication";
 import crypto from "@app/config/crypto";
 import db from "@app/config/database";
 import services from "@app/config/services";
-import kafka from "@app/config/kafka";
 import settings from "@app/config/settings";
 
 import mailer from "./mailer";
 
-export default [app, db, crypto, settings, kafka, services, mailer, auth];
+export default [app, db, crypto, settings,  services, mailer, auth];
